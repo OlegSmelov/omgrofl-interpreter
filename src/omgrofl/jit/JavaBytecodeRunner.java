58d385c084cce48f4a62359eb231d24d11dd5e90
Oleg Smelov <masterprog1@gmail.com>
2014-03-21 17:03:22 +0200

    Fix JavaBytecodeRunner

--- src/omgrofl/jit/JavaBytecodeRunner.java
+++ src/omgrofl/jit/JavaBytecodeRunner.java
@@ -3,6 +3,7 @@ package omgrofl.jit;
 import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.lang.reflect.Method;
 
 public class JavaBytecodeRunner extends ClassLoader {
     
@@ -12,7 +13,13 @@ public class JavaBytecodeRunner extends ClassLoader {
         try {
             JavaBytecodeRunner bytecodeRunner = new JavaBytecodeRunner();
             Class newClass = bytecodeRunner.defineClass(className, bytecode, 0, bytecode.length);
-            newClass.getMethods()[0].invoke(null, new Object[] {null});
+            Method[] methods = newClass.getMethods();
+            for (Method method : methods) {
+                if ("main".equals(method.getName())) {
+                    method.invoke(null, new Object[] {null});
+                    break;
+                }
+            }
         } catch (IllegalAccessException ex) {
             Logger.getLogger(JavaBytecodeRunner.class.getName()).log(Level.SEVERE, null, ex);
         } catch (IllegalArgumentException ex) {
