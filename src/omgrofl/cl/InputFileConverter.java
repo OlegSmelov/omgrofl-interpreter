805b35c56c893b24bac00cf6b3b3b6009fb01d39
Oleg Smelov <OlegSmelov@users.noreply.github.com>
2018-03-02 23:40:02 +0200

    Refactor InputFileConverter

--- src/omgrofl/cl/InputFileConverter.java
+++ src/omgrofl/cl/InputFileConverter.java
@@ -5,18 +5,23 @@ import com.beust.jcommander.ParameterException;
 import java.io.File;
 
 public class InputFileConverter implements IStringConverter<File> {
+
     @Override
     public File convert(String filename) {
         File file = new File(filename);
-        if (file.exists()) {
-            if (file.isFile()) {
-                if (!file.canRead())
-                    throw new ParameterException("Can't read from the file");
-                return file;
-            }
-            throw new ParameterException("File parameter should be a file");
-        } else {
+
+        if (!file.exists()) {
             throw new ParameterException("File doesn't exist");
         }
+
+        if (!file.isFile()) {
+            throw new ParameterException("File parameter should be a file");
+        }
+
+        if (!file.canRead()) {
+            throw new ParameterException("Can't read from the file");
+        }
+
+        return file;
     }
 }
